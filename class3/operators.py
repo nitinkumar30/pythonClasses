151b4333986bfeaae80c32155727d98066e1e8a0
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2023-01-11 16:15:34 +0530

    Add files via upload

---
+++ class3/operators.py
@@ -0,0 +1,86 @@
+# Operators
+
+# Arithmetic operators
+
+# +, -, *, /, %, **, //
+
+# print(3+5) # 8
+# print(5-3) # 2
+# print(5*3)  # 15
+# print(6/3) # 2.0
+# print(6%5) # 1
+# print(3**2) # 9
+# print(6//3) # 2
+
+# Assignment operators
+
+# =, +=, -=, *=, /=, %=, **=, //=, &=, |=, ^=
+
+# a = 5
+# print(a)
+# a += 1
+# print(a)
+# a -= 2
+# print(a) # 4
+# a *= 3 # a = a * 3  # 4 = 4 * 3  # 4 = 12
+# print(a)
+# a /= 2  # 12 = 12 / 2 # 6.0
+# print(a)
+# b = 100
+# print(b)
+# b %= 7  # 100 % 7 = 7 * 14 = 98 (100-98)
+# print(b)
+# b **= 2  # 2 ** 2 = 4
+# print(b)
+# b //= 3  # 4 / 3 = 1
+# print(b)
+
+
+# Comparison operators
+
+# ==, <=, >=, !=, <, >
+
+# print(2 == 2)
+# print(2 <= 2)
+# print(5 <= 2)
+# print(2 != 3)
+# print(4 == 4)  # False
+# print(4 < 9)
+
+
+# Logical operators
+
+# and, or, not
+
+# print(1 < 2 and 4 < 6) # True
+# print(1 < 2 or 1 > 2)  # True
+# print(1 not in [1, 2])  # False
+
+
+# Identity operators
+
+# is, is not
+
+# x = 2
+# y = 4
+# print(x is y)  # x == y
+# print(x is not y)  # x != y
+
+
+# Membership operators
+
+# not in, in
+
+# lst = [1, 2, 3, 4, 5]
+# print(4 in lst)  # true
+# print(6 in lst)  # false
+# print(2.0 in lst)  # false
+# print([2, 3] in lst)  # true
+# print([2] in lst)
+# print(2 in lst)
+
+
+# Bitwise operators
+
+# &, |, ^, ~, <<, >>
+
