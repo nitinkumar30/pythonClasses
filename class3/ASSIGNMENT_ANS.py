b1ee3a6f889332f999304f4a07dfa1ba7ea7aa8f
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2023-01-11 16:14:02 +0530

    Add files via upload

---
+++ class3/ASSIGNMENT_ANS.py
@@ -0,0 +1,7 @@
+# Q 3.
+
+a, b, c = 'Nitin', 'Yash', 'Anji'
+# # Nitin was going to the party with Yash where he met his friend Anji
+
+print("{} was going to the party with {} where he met his friend {}".format(a, b, c))
+
